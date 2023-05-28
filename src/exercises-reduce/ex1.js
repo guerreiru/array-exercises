fbfb021bae7b8ecf43cd2a7857b71e9f0e946d10
Fernando Guerreiro <fernando.rodrigues@prodap.com.br>
2023-05-28 12:43:32 -0300

    fix: correcting formatting in exercises-reduce/ex1 file

--- src/exercises-reduce/ex1.js
+++ src/exercises-reduce/ex1.js
@@ -8,10 +8,10 @@ const sum = arrayToSum.reduce((acc, num) => acc + num, 0);
 // 2. Find the maximum value in an array:
 const arrayToFindGreatestValue = [10, 5, 20, 15, 30];
 const greatestValue = arrayToFindGreatestValue.reduce((acc, num) => {
-if (num > acc) {
-return num;
-}
-return acc;
+  if (num > acc) {
+    return num;
+  }
+  return acc;
 });
 // console.log(greatestValue);
 
@@ -23,20 +23,20 @@ const concatenatedString = arrayToConcatenate.join(' ');
 // 4. Count the occurrence of each element in an array:
 const occurrenceNumbers = [1, 2, 3, 2, 1, 1, 3, 4, 5, 4, 3, 2];
 const occurrences = occurrenceNumbers.reduce((acc, num) => {
-return {
-...acc,
-     [num]: (acc[num] || 0) + 1
-}
+  return {
+    ...acc,
+    [num]: (acc[num] || 0) + 1
+  }
 }, {})
 // console.log(occurrences);
 
 // 5. Group objects based on a specific property:
 const arrayGroupBasedProperty = [
-{ id: 1, category: 'A' },
-{ id: 2, category: 'B' },
-{ id: 3, category: 'A' },
-{ id: 4, category: 'C' },
-{ id: 5, category: 'B' }
+  { id: 1, category: 'A' },
+  { id: 2, category: 'B' },
+  { id: 3, category: 'A' },
+  { id: 4, category: 'C' },
+  { id: 5, category: 'B' }
 ];
 
 const arrayGroupByCategory = groupBy(arrayGroupBasedProperty, 'category')
